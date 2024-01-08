56a26bb22a329439599af0ae59f4664e172635f1
Saurabh  Shringarpure <saurabh-shringarpure@users.noreply.github.com>
2024-01-08 12:01:30 +0900

    updated CP projection

--- MATLAB/CP.m
+++ MATLAB/CP.m
@@ -1,7 +1,7 @@
 %% CP projection
 function y = CP(x)
     x = reshape(x, [8,8]); 
-    [U, S, V] = svd(x);
-    S = S - min(0, min(S(:))); % removes negative singular values making a lower rank CP matrix
-    y = U*S*V';
+    [V, D] = eig(x);
+    D = D - min(0,D); % removes negative eigenvalues making a lower rank CP matrix
+    y = V*D/V;
 end
