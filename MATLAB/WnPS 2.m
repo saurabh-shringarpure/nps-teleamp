b4f439436378fc1eeba679f530004fdb35f088d9
Saurabh  Shringarpure <saurabh-shringarpure@users.noreply.github.com>
2024-02-08 18:28:58 +0900

    remove unused functions

--- MATLAB/WnPS 2.m
+++
@@ -1,4 +0,0 @@
-%% process matrix for a qubit encoded on cat states after nPS, passing through lossy channel
-function y = WnPS(alpha, gamma, n)
- y = (1-mod(n,2))*Weven(alpha, gamma)+(mod(n,2))*Wodd(alpha, gamma);
-end
